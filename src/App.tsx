710b0d87bcb6fe284ac5bba29f891c286ac85bea
aiandrox <aiandrox7@gmail.com>
2024-03-10 13:03:50 +0900

    それっぽくした

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import themes from "./themes.json";
+import "./App.css";
 
-function App() {
+type ThemeType = {
+  title: string;
+  max: string;
+  min: string;
+};
+
+const App: React.FC = () => {
+  const [sampleThemes, setSampleThemes] = useState<ThemeType[]>([]);
+  const [selectedTheme, setSelectedTheme] = useState<ThemeType | null>(null);
+
+  useEffect(() => {
+    const randomThemes = themes.sort(() => 0.5 - Math.random()).slice(0, 3);
+    setSampleThemes(randomThemes);
+  }, []);
+
+  const handleClick = (theme: ThemeType) => {
+    setSelectedTheme(theme);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ maxWidth: "600px", margin: "0 auto" }}>
+      {selectedTheme ? (
+        <div>
+          <h1>{selectedTheme.title}</h1>
+          <p>
+            Max: {selectedTheme.max}, Min: {selectedTheme.min}
+          </p>
+          <button onClick={() => setSelectedTheme(null)}>戻る</button>
+        </div>
+      ) : (
+        <div>
+          <h1>お題</h1>
+          <ul>
+            {sampleThemes.map((theme, index) => (
+              <li key={index} onClick={() => handleClick(theme)}>
+                {theme.title}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
