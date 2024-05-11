c243aba02b59d6e57726e6480634fb2d91544131
aiandrox <aiandrox7@gmail.com>
2024-05-11 17:35:00 +0900

    ページネーションできるようにする

--- src/components/Top/Top.component.tsx
+++ src/components/Top/Top.component.tsx
@@ -1,13 +1,30 @@
 import themes from "../../themes.json";
-import { useState } from "react";
-import { Theme } from "../../models";
+import { useEffect, useState } from "react";
+import { Theme, PAGE_SIZE } from "../../models";
 import { ThemeItemComponent } from "./ThemeItem.component";
 import * as Styles from "./Top.styles";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation, Link } from "react-router-dom";
 
 export const TopComponent = () => {
   const navigate = useNavigate();
+  const search = useLocation().search;
   const [sampleThemes, setSampleThemes] = useState<Theme[]>([]);
+  const [paginatedThemes, setPaginatedThemes] = useState<Theme[]>([]);
+  const [page, setPage] = useState(1);
+
+  const query = new URLSearchParams(search);
+  const queryPage = Number(query.get("page"));
+
+  useEffect(() => {
+    setPage(Number(queryPage) || 1);
+  }, [queryPage]);
+
+  useEffect(() => {
+    const start = PAGE_SIZE * (page - 1);
+    const end = start + PAGE_SIZE;
+    const slicedThemes = themes.slice(start, end);
+    setPaginatedThemes(slicedThemes);
+  }, [page]);
 
   const selectRandomThemes = () => {
     const randomThemes = [...themes].sort(() => 0.5 - Math.random()).slice(0, 3);
@@ -35,8 +52,15 @@ export const TopComponent = () => {
         </>
       )}
       <ul>
-        {themes.map((theme) => (
-          <ThemeItemComponent onClickTheme={onClickTheme} theme={theme} />
+        {paginatedThemes.map((theme) => (
+          <ThemeItemComponent key={theme.id} onClickTheme={onClickTheme} theme={theme} />
+        ))}
+      </ul>
+      <ul>
+        {Array.from({ length: Math.ceil(themes.length / PAGE_SIZE) }, (_, i) => i + 1).map((num) => (
+          <Link key={num} to={`?page=${num}`}>
+            {num}
+          </Link>
         ))}
       </ul>
     </>
