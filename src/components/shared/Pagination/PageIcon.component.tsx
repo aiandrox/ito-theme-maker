a86eaae946195781a1d81861c014b07712e3d213
aiandrox <aiandrox7@gmail.com>
2024-05-11 19:12:56 +0900

    前へ、次へを追加

---
+++ src/components/shared/Pagination/PageIcon.component.tsx
@@ -0,0 +1,15 @@
+import * as Styles from "./Pagination.styles";
+
+type Props = {
+  disabled?: boolean;
+  onClick: () => void;
+  children: React.ReactNode;
+};
+
+export const PageIcon = ({ disabled, onClick, children }: Props) => {
+  return (
+    <Styles.Li disabled={disabled} onClick={onClick}>
+      {children}
+    </Styles.Li>
+  );
+};
--- src/components/shared/Pagination/Pagination.component.tsx
+++ src/components/shared/Pagination/Pagination.component.tsx
@@ -1,4 +1,5 @@
 import * as Styles from "./Pagination.styles";
+import { PageIcon } from "./PageIcon.component";
 
 type Props = {
   totalPage: number;
@@ -9,11 +10,17 @@ type Props = {
 export const Pagination = ({ totalPage, currentPage, onChangePage }: Props) => {
   return (
     <Styles.Ol>
+      <PageIcon disabled={currentPage === 1} onClick={onChangePage(currentPage - 1)}>
+        ←
+      </PageIcon>
       {Array.from({ length: totalPage }, (_, i) => i + 1).map((page) => (
         <Styles.Li disabled={page === currentPage} key={page} onClick={onChangePage(page)}>
           {page}
         </Styles.Li>
       ))}
+      <PageIcon disabled={currentPage === totalPage} onClick={onChangePage(currentPage + 1)}>
+        →
+      </PageIcon>
     </Styles.Ol>
   );
 };
